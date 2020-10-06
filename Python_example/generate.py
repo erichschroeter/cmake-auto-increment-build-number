f7688991a92b5491e45b60659282375df60d3460
Erich Schroeter <erichschroeter@gmail.com>
2020-10-06 17:20:15 -0500

    Fixed generate always running

--- Python_example/generate.py
+++ Python_example/generate.py
@@ -1,44 +1,61 @@
 '''
 USAGE:
-  generate.py [-o <file>] <config>
+  generate.py [-o <dir>] <config>
 
 OPTIONS:
   -v, --version                            Print the version
   -h, --help                               Print this help menu
-  -o, --output <file>                      Write output to this file
+  -o, --output <dir>                       Write output files to this directory
 '''
+import os
 import sys
 import json
 from textwrap import dedent
 from datetime import datetime
 from docopt import docopt
 
-def write_version_header(out, config):
-    now = datetime.now()
-    out.write(dedent('''\
-    #ifndef {prefix}VERSION_H
-    #define {prefix}VERSION_H
+class CppGenerator:
+    def __init__(self, version):
+        self.decorated = version
 
-    #define {prefix}VERSION_MAJOR {major}
-    #define {prefix}VERSION_MINOR {minor}
-    #define {prefix}VERSION_PATCH {patch}
-    #define {prefix}VERSION_BUILD {build}
-    #define {prefix}VERSION_META "{meta}"
-    #define {prefix}VERSION "{major}.{minor}.{patch}.{build}{meta}"
-    #define {prefix}COMPILE_DATE "{date}"
-    #define {prefix}COMPILE_TIME "{time}"
+    def header(self):
+        now = datetime.now()
+        return dedent('''\
+            #ifndef {prefix}VERSION_H
+            #define {prefix}VERSION_H
 
-    #endif /* {prefix}VERSION_H */
-    ''').format(
-        prefix = config['prefix'] if 'prefix' in config else '',
-        major = config['major'] if 'major' in config else 0,
-        minor = config['minor'] if 'minor' in config else 0,
-        patch = config['patch'] if 'patch' in config else 0,
-        build = config['build'] if 'build' in config else 0,
-        meta = config['meta'] if 'meta' in config else '',
-        date = now.strftime('%d/%m/%Y'),
-        time = now.strftime('%H:%M:%S')
-    ))
+            #define {prefix}VERSION_MAJOR {major}
+            #define {prefix}VERSION_MINOR {minor}
+            #define {prefix}VERSION_PATCH {patch}
+            #define {prefix}VERSION_BUILD {build}
+            #define {prefix}VERSION_META "{meta}"
+            #define {prefix}VERSION "{major}.{minor}.{patch}.{build}{meta}"
+            #define {prefix}COMPILE_DATE "{date}"
+            #define {prefix}COMPILE_TIME "{time}"
+
+            const char * hello_world();
+
+            #endif /* {prefix}VERSION_H */
+            '''.format(
+                prefix = self.decorated['prefix'] if 'prefix' in self.decorated else '',
+                major = self.decorated['major'] if 'major' in self.decorated else 0,
+                minor = self.decorated['minor'] if 'minor' in self.decorated else 0,
+                patch = self.decorated['patch'] if 'patch' in self.decorated else 0,
+                build = self.decorated['build'] if 'build' in self.decorated else 0,
+                meta = self.decorated['meta'] if 'meta' in self.decorated else '',
+                date = now.strftime('%d/%m/%Y'),
+                time = now.strftime('%H:%M:%S')
+            ))
+
+    def source(self):
+        return dedent('''\
+            #include "version.h"
+
+            const char * hello_world()
+            {
+                return "Hello World";
+            } 
+            ''')
 
 def main():
     argv = docopt(__doc__, version="1.0")
@@ -46,11 +63,17 @@ def main():
     if argv['<config>'] is not None:
         with open(argv['<config>']) as config_file:
             config = json.load(config_file)
+            gen = CppGenerator(config)
             if argv['--output']:
-                with open(argv['--output'], 'w') as out:
-                    write_version_header(out, config)
+                header_file = os.path.join(argv['--output'], 'version.h')
+                source_file = os.path.join(argv['--output'], 'version.c')
+                with open(header_file, 'w') as out:
+                    out.write(gen.header())
+                with open(source_file, 'w') as out:
+                    out.write(gen.source())
             else:
-                write_version_header(sys.stdout, config)
+                print(gen.header())
+                print(gen.source())
 
 if __name__ == "__main__":
     main()
--- Python_example/src/main.c
+++ Python_example/src/main.c
@@ -5,6 +5,8 @@
 int main(int argc, char **argv)
 {
 	printf("version: %s\n", EXAMPLE_VERSION);
+
 	printf("compiled: %s %s\n", EXAMPLE_COMPILE_DATE, EXAMPLE_COMPILE_TIME);
+	printf("%s\n", hello_world());
 	return 0;
 }
